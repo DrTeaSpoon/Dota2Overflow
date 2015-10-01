d0b2047e0e006f8c8f254928e3a2ed29d31fb014
DrTeaSpoon <otsowb@gmail.com>
2015-10-02 00:07:44 +0300

    Fixes

--- game/dota_addons/dota2overflow/scripts/vscripts/lua_abilities/passives/burner/passive.lua
+++ game/dota_addons/dota2overflow/scripts/vscripts/lua_abilities/passives/burner/passive.lua
@@ -2,6 +2,7 @@ if burner == nil then
 	burner = class({})
 end
 
+LinkLuaModifier("element_fire", "lua_mods/element_fire", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier( "burner_mod", "lua_abilities/passives/burner/p_mod.lua", LUA_MODIFIER_MOTION_NONE )
 
 function burner:GetBehavior() 
