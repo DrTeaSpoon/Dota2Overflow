cbb8f414b7cb3e88636923113240a781369418cd
DrTeaSpoon <otsowb@gmail.com>
2016-01-13 23:41:34 +0200

    Fissure

--- game/dota_addons/dota2overflow/scripts/vscripts/heroes/hero_earthshaker/earthshaker_fissure.lua
+++ game/dota_addons/dota2overflow/scripts/vscripts/heroes/hero_earthshaker/earthshaker_fissure.lua
@@ -3,6 +3,7 @@ if earthshaker_fissure_lua == nil then
 end
 
 LinkLuaModifier( "earthshaker_fissure_lua_stun", "heroes/hero_earthshaker/earthshaker_fissure_stun.lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "earthshaker_fissure_lua_thinker", "heroes/hero_earthshaker/earthshaker_fissure_think.lua", LUA_MODIFIER_MOTION_NONE )
 
 function earthshaker_fissure_lua:GetBehavior() 
 	local behav = DOTA_ABILITY_BEHAVIOR_POINT + DOTA_ABILITY_BEHAVIOR_IGNORE_BACKSWING
@@ -16,22 +17,46 @@ function earthshaker_fissure_lua:OnSpellStart()
 	local vForward = self:GetCursorPosition() - hCaster:GetAbsOrigin()
 	vForward = vForward:Normalized() 
 	local nDuration	= self:GetSpecialValueFor( "fissure_duration" )
+	local nStun	= self:GetSpecialValueFor( "stun_duration" )
 	local nRadius	= self:GetSpecialValueFor( "fissure_radius" )
 	local vStartPos = hCaster:GetAbsOrigin()
 	local vEndPos = vStartPos + vForward * nLenght
+	vStartPos = vStartPos + vForward * 64
 	EmitSoundOnLocationWithCaster(vEndPos, "Hero_EarthShaker.Fissure.Cast", hCaster )
 	local particleName = "particles/units/heroes/hero_earthshaker/earthshaker_fissure.vpcf"
 	local nFXIndex = ParticleManager:CreateParticle( particleName, PATTACH_ABSORIGIN, hCaster )
-	print(nFXIndex)
 	ParticleManager:SetParticleControl( nFXIndex, 0, vStartPos )
 	ParticleManager:SetParticleControl( nFXIndex, 0, vStartPos )
 	ParticleManager:SetParticleControl( nFXIndex, 1, vEndPos )
 	ParticleManager:SetParticleControl( nFXIndex, 2, Vector( nDuration, 0, 0 ) )
-	local hObs = Entities:CreateByClassname("point_simple_obstruction") 
-	hObs:SetOrigin(vStartPos + Vector(0,0,0))
-	DebugDrawCircle(hObs:GetAbsOrigin(), Vector(255,255,255), 1, 50, true, 5) 
 	
-	--hObs:SetSize( Vector( nRadius, nRadius, nRadius ), Vector( nRadius, nRadius, nRadius )) 
-	hObs:SetEnabled(true, true)
-	GameRules:GetGameModeEntity():ClientLoadGridNav()
+	local numBoulder = math.floor( nLenght / (32*2) ) + 1
+	local stepLength = nLenght / ( numBoulder - 1 )
+	for i=1, numBoulder do
+		local boulderPos = vStartPos + vForward * (i-1) * stepLength
+		self:SpawnBoulder(boulderPos,nDuration)
+	end
+	local tVictims = FindUnitsInLine( hCaster:GetTeamNumber(), vStartPos, vEndPos , nil, nRadius, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, FIND_ANY_ORDER  )
+	local nDamageType = self:GetAbilityDamageType()
+	local nDamage = self:GetAbilityDamage()
+	local damage = {
+		attacker = hCaster,
+		damage = nDamage,
+		damage_type = nDamageType,
+		ability = self
+	}
+	for k,v in pairs(tVictims) do
+		FindClearSpaceForUnit(v, v:GetAbsOrigin(), true)
+		if v:GetTeam() ~= hCaster:GetTeam() then
+			v:AddNewModifier( hCaster, self, "earthshaker_fissure_lua_stun", { duration = nStun } )
+			damage.victim = v
+			ApplyDamage( damage )
+		end
+	end
+end
+
+function earthshaker_fissure_lua:SpawnBoulder(vPos,nDuration)
+	local hCaster = self:GetCaster()
+	--DebugDrawCircle(vPos, Vector(255,255,255), 1, 50, true, nDuration) 
+	CreateModifierThinker(hCaster, self, "earthshaker_fissure_lua_thinker", {duration = nDuration}, vPos, hCaster:GetTeam(), true) 
 end
---
+++ game/dota_addons/dota2overflow/scripts/vscripts/heroes/hero_earthshaker/earthshaker_fissure_think.lua
@@ -0,0 +1,39 @@
+if earthshaker_fissure_lua_thinker == nil then
+	earthshaker_fissure_lua_thinker = class({})
+end
+ 
+function earthshaker_fissure_lua_thinker:OnCreated( kv )	
+	if IsServer() then
+	end
+end
+
+function earthshaker_fissure_lua_thinker:OnRefresh( kv )	
+	if IsServer() then
+	end
+end
+
+function earthshaker_fissure_lua_thinker:OnDestroy()
+	if IsServer() then
+		UTIL_Remove( self:GetParent() )
+	end
+end
+
+function earthshaker_fissure_lua_thinker:IsHidden()
+	return true
+end
+
+function earthshaker_fissure_lua_thinker:IsPurgable() 
+	return false
+end
+
+function earthshaker_fissure_lua_thinker:IsPurgeException()
+	return false
+end
+
+function earthshaker_fissure_lua_thinker:GetAttributes()
+	return MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE 
+end
+
+function earthshaker_fissure_lua_thinker:AllowIllusionDuplicate() 
+	return false
+end
